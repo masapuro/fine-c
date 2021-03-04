4e311e42d969c78c7cf8bd68c4ccc971bbf047ee
masapuro <67581248+masapuro@users.noreply.github.com>
2021-03-03 22:03:30 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,47 @@
     pauseOnFocus: false,
     pauseOnHover: false,
   });
+  $("#wrapper").click(function () {
+    $(this).toggleClass("active");
+
+    if ($(this).hasClass("active")) {
+      $("#humburger").slideDown();
+    } else {
+      $("#humburger").slideUp();
+    }
+  });
+  (() => {
+    const sample = document.querySelectorAll('.sample');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.classList.add('sample-animation');
+        } else {
+          entry.target.classList.remove('sample-animation');
+        }
+      });
+    });
+    
+    sample.forEach(img => {
+      observer.observe(img);
+    });
+  })();
+  (() => {
+    const sample = document.querySelectorAll('.sample2');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.classList.add('sample-animation');
+        } else {
+          entry.target.classList.remove('sample-animation');
+        }
+      });
+    });
+    
+    sample.forEach(img => {
+      observer.observe(img);
+    });
+  })();
+  
